a5a8c8d6586e00ad395aea04b1fe0cfb781db997
Guillaume <ronnel.guillaume@proton.me>
2024-01-10 18:41:00 +0100

    devloppement finished

--- index.js
+++ index.js
@@ -8,25 +8,38 @@ const lb_password= process.env.LETTERBOXD_PASSWORD;
 
 
 (async () => {
-   const browser = await puppeteer.launch({ headless: false });
+   const prompt = require("prompt-sync")({ sigint: true });
+   const date = prompt("Chose the date to remove (year (YYYY), month (YYYY/MM) or day (YYYY/MM/DD): ");
+   console.log(`Removing ${date} \n`);
+
+
+   const browser = await puppeteer.launch({ headless: false});
    const page = await browser.newPage();
    await page.setViewport({ width: 1620, height: 700 });
    await page.goto('https://letterboxd.com/', { waitUntil: "load" });
 
-   await page.waitForSelector('.fc-cta-consent');
+   await page.waitForSelector('.fc-cta-consent').catch(() => {
+      console.log(".fc-cta-consent")
+   });
 
    await page.evaluateHandle(() => {
       document.querySelector('.fc-cta-consent').click()
    }).catch(() => console.log('Cannot find .fc-cta-consent'))
    
-   await page.waitForSelector('.sign-in-menu');
+   await page.waitForSelector('.sign-in-menu').catch(() => {
+      console.log(".sign-in-menu wait failed")
+   });
 
    await page.evaluateHandle(() => {
       document.querySelector('.sign-in-menu>a').click() 
    }).catch(() => console.log('Cannot find Signup'))
 
-   await page.waitForSelector('#username');
-   await page.waitForSelector('#password');
+   await page.waitForSelector('#username').catch(() => {
+      console.log("#username wait failed")
+   });
+   await page.waitForSelector('#password').catch(() => {
+      console.log("#password wait failed")
+   });
 
    await page.evaluateHandle((lb_username, lb_password) => {
       document.querySelector('#username').value = lb_username
@@ -35,41 +48,75 @@ const lb_password= process.env.LETTERBOXD_PASSWORD;
    }, lb_username, lb_password).catch(() => console.log('Cannot log'))
 
 
-   await page.waitForSelector('#add-new-button');
-   await page.goto(`https://letterboxd.com/${lb_username}/films/diary/for/2022/12/28/`, { waitUntil: "load" });
+   await page.waitForSelector('#add-new-button').catch(() => {
+      console.log("#add-new-button wait failed")
+   });
+   await page.goto(`https://letterboxd.com/${lb_username}/films/diary/for/${date}/`, { waitUntil: "load" });
 
-   await page.waitForSelector('.paginate-pages');
-   let number_pages =0 
-   await page.evaluateHandle((number_pages) => {
-      const last_page = document.querySelectorAll('.next').length
-      number_pages = parseFloat(document.querySelectorAll('.paginate-pages>ul>li')[last_page-1].innerText)
-   }, number_pages)
+   await page.waitForSelector('.pagination').catch(() => {
+      console.log(".pagination wait failed")
+   })
+   await page.waitForSelector('.paginate-pages>ul>li').catch(() => {
+      console.log(".paginate-pages>ul>li wait failed (possibly because there's only 1 page)")
+   })
 
-   await page.waitForTimeout(500);
-   console.log(number_pages)
+   let number_pages = await page.evaluate(() => {
+      const last_page = document.querySelectorAll('.paginate-pages>ul>li').length
+
+      if (last_page == 0 ) {
+         return 1
+      } else {
+         return document.querySelectorAll('.paginate-pages>ul>li')[last_page-1].innerText
+      }
+   })
 
    for (let i = 0; i<number_pages; i++) {
       if (i>0) {
+         await page.evaluate(() => {
+            location.reload(true)
+         }).catch(() => console.log('Cannot refresh'))
+         await page.waitForSelector('.pagination').catch(() => {
+            console.log(".pagination wait failed")
+         });
+
+         await page.waitForTimeout(500)
+         console.log("\nPage", i+1, '\n')
+      }
+
+      let number_items= await page.evaluate(() => {
+         return document.querySelectorAll('.diary-entry-edit.shown-for-owner').length
+      } )
+
+      if (number_items == 0) {
+         console.log("No log on this date")
+         page.close()
+         browser.close()
+      }
+
+      for (let j = 0; j<number_items; j++) {
+         console.log("Deleting movie", j+1, 'of page', i+1)
+
+         await page.evaluateHandle((j) => {
+            document.querySelectorAll('.diary-entry-edit.shown-for-owner>a')[j].click()
+         },j).catch(error => console.log('Cannot click on movie edit', j+1));
+
+         await page.waitForSelector('.specify-date-label-open>i').catch(() => {
+            console.log("specify-date-label-open>i wait failed")
+         })
+         await page.waitForSelector('#diary-entry-submit-button').catch(() => {
+            console.log("#diary-entry-submit-button wait failed")
+         })
+
          await page.evaluateHandle(() => {
-            document.querySelector('.next').click()
-         }).catch(() => console.log('Cannot click next'))
-         await page.waitForSelector('.paginate-pages');
+            document.querySelector('.specify-date-label-open>i').click() 
+            document.querySelector('#diary-entry-submit-button').click() 
+         }).catch(error => console.log('Cannot edit movie', j+1));
+
+
+         await page.waitForTimeout(1000)
       }
    }
 
-   // let next = null
-   // do {
-   //    await page.evaluateHandle(() => {
-   //       document.querySelector('.pagination>div.paginate-nextprev>a.next').click()
-   //    })
-
-   //    await page.waitForSelector('.paginate-pages');
-   //    next = await page.evaluateHandle((next) => {
-   //       return document.querySelector('.paginate-disabled>span.next')
-   //    }, next)
-
-   //    console.log(next)
-
-   // } while ( next == 1 )
+   process.exit()
 }) ()
 
